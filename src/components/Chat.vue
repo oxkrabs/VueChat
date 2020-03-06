fe8976ab1fe7714ab4437e9fa984170b7788ee4a
Andreas Oxinos <oxinosa@gmail.com>
2020-03-06 12:20:52 +0100

    init

---
+++ src/components/Chat.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    Chat
+    <div>
+      <form>
+        <input type="text" v-model="message" />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+    <div class="chat-container">
+      <div class="chat-box"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chat",
+  data: () => ({
+    message: ""
+  })
+};
+</script>
+
+<style scoped>
+.chat-container {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+}
+.chat-box {
+  width: 500px;
+  height: 500px;
+  outline: 1px solid black;
+}
+</style>
